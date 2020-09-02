248410ba5211c9fda8f34cf9817d0209b308450c
Henry <henryg.gruning@gmail.com>
2020-09-02 10:24:03 +0300

    Lisasin:    * Laulude lisamise koodi osakese.    * Laulude järjekorda panemise osakese.

--- PaavoUus.py
+++ PaavoUus.py
@@ -1,4 +1,4 @@
-class laul():
+class laulA():
     def __init__(self, lauluNimi):
         self.lauluNimi = lauluNimi
 
@@ -6,7 +6,7 @@ class artist():
     def __init__(self, artistiNimi):
         self.artistiNimi = artistiNimi
 
-class album():
+class albumA():
     def __init__(self, albumiNimi):
         self.albumiNimi = albumiNimi
 
@@ -16,17 +16,49 @@ albumid = []
 def albumiteKriipsud():
     for rida in fail:
         elemendid = rida.split("\t")
-        esineja = elemendid[0]
-        album = elemendid[1]
-        albumid.append(album)
+        esineja = artist(elemendid[0])
+        album = albumA(elemendid[1])
+        albumid.append(elemendid[1])
         aasta = elemendid[2]
-        laul = elemendid[3]
+        laul = laulA(elemendid[3])
         if len(albumid) > 1:
             if albumid[-2] != albumid[-1]:
                 print()
                 print("--------------------------------------------")
                 print()
-        print(rida)
+        print(esineja.artistiNimi, album.albumiNimi, aasta, laul.lauluNimi)
+
+b = input("Kas soovite lisada laule juurde?: ")
+
+if b == "jah":
+    seis = "aktiivne"
+    grupp = input("Sisestage esitaja nimi: ")
+    album = input("Sisestage albumi nimi: ")
+    aasta = input("Sisestage aasta: ")
+    # Tsükkel kestab kuni seis = aktiivne.
+    while seis == "aktiivne":
+
+        laul = input("Sisestage laulu pealkiri: ")
+        # Liidab kokku esitaja, albumi, aasta ning laulu. Vahel on tabulutsioon.
+        laulupealkiri = grupp.title() + "\t" + album.title() + "\t" + aasta + "\t" + laul.title()
+        fail = open("albumid.txt", "a", encoding="UTF-8")
+        # Lisab nimekirja uue plaadi andmed.
+        fail.write("\n" + laulupealkiri)
+
+        # Küsib, kas kasutaja soovib veel laule sisestada.
+        b = input("Kas soovite veel laule lisada? jah/ei: ")
+        if b == "jah":
+            # Seis püsib aktiivne.
+            seis = "aktiivne"
+        else:
+            # Seisu muutmine inaktiivseks.
+            seis = "inaktiivne"
+            fail.close()
+
+# Kui vastus ei ole jah, siis liigub järgmise küsimuse juurde.
+else:
+    pass
+
 
 vastus = input("Kas soovite albumite nimekirja näha?: ")
 if vastus == "jah":
